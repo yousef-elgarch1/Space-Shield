9626c9e1c8c4e75a15d405e8facbd2222ddabf12
Younes Bazzaoui <younrun.rocka@gmail.com>
2025-04-08 18:06:05 +0100

    this second commit is about finding out how threejs works i have problem with versioning going to see it through next time inchlh

--- src/main/java/com/debriswatch/debristracker/controller/HomeController.java
+++ src/main/java/com/debriswatch/debristracker/controller/HomeController.java
@@ -2,14 +2,12 @@ package com.debriswatch.debristracker.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.ui.Model;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String home(Model model) {
-        model.addAttribute("message", "Welcome to the Debris Tracking System !");
-        return "index"; // This refers to a template file named index.html
+    public String homePage() {
+        return "index"; // Loads templates/index.html via Thymeleaf
     }
 }
